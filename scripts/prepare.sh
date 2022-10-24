b4bc736439993c331c8b11a362f60abe54b0519a
jinseong.cho <jinseong.cho@linecorp.com>
2022-10-25 00:03:16 +0900

    feat: chnage script not to use confio

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -8,3 +8,8 @@ for dir in $DIRS; do
   rm -rf "$dir"
   cp -R "./build/$dir" ./
 done
+
+rm proofs.d.ts proofs.js proofs.js.map
+cp ./build/proofs.d.ts ./
+cp ./build/proofs.js ./
+cp ./build/proofs.js.map ./
