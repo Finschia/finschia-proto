15c0c9b42b24887451b4d85d2376f72679c862ab
jinseong.cho <jinseong.cho@linecorp.com>
2023-03-13 20:24:10 +0900

    fix:enable compile with kt

--- java/app/build.gradle.kts
+++ java/app/build.gradle.kts
@@ -7,7 +7,7 @@ import org.gradle.api.JavaVersion.VERSION_1_8
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
+    id("org.jetbrains.kotlin.jvm") version "1.8.10"
 
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
