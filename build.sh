14cb34c69ee56a7cbf320f41a0e1adb6fe1ab4b4
jinseong.cho <jinseong.cho@linecorp.com>
2023-04-11 15:09:25 +0900

    feat: add checkout in build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,29 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+# shellcheck source=./env
+# shellcheck disable=SC1091
+source "$SCRIPT_DIR"/env
+
 echo "module update"
 git submodule update --init --remote
 
+echo "checkout to desired version"
+echo "checkout to lbm-sdk $LBM_SDK_VERSION"
+cd ./repositories/lbm-sdk 
+git checkout "$LBM_SDK_VERSION"
+
+echo "checkout to wasmd $WASMD_VERSION"
+cd ../wasmd
+git checkout "$WASMD_VERSION"
+
+echo "checkout to ibc-go $IBC_GO_VERSION"
+cd ../ibc-go
+git checkout "$IBC_GO_VERSION"
+
 echo "build js types"
-cd ./js
+cd ../../js
 # install dependencies
 yarn install
 # Generate .ts files into ./src
