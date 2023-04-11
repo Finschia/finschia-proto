3d3b66abaef79302350518bf69d66086f4b4d99e
jinseong.cho <jinseong.cho@linecorp.com>
2023-04-11 16:17:23 +0900

    apply feedback: simplify source path

--- build.sh
+++ build.sh
@@ -2,10 +2,7 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
-SCRIPT_DIR="$(realpath "$(dirname "$0")")"
-# shellcheck source=./env
-# shellcheck disable=SC1091
-source "$SCRIPT_DIR"/env
+source ./env
 
 echo "module update"
 git submodule update --init --remote
